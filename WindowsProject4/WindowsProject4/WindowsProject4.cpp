37715ad2fec60f4840ef7c734f8f52cf50282c40
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-07 22:05:31 +0300

    Fourth task. WinApi laboratory work.

---
+++ WindowsProject4/WindowsProject4/WindowsProject4.cpp
@@ -0,0 +1,284 @@
+﻿// WindowsProject4.cpp : Определяет точку входа для приложения.
+//
+
+#include "stdafx.h"
+#include "WindowsProject4.h"
+#include "string.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void LoadAndBlitBitmap(LPCWSTR fileName, HDC hdc, int x, int y);
+
+
+int currentX = 0, currentY = 0, destinationX = 0, destinationY = 0;
+int animation = 0;
+int localAnimation = 0;
+bool isMoving = false;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WINDOWSPROJECT4, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT4));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT4));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT4);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+	case WM_CREATE:
+		{
+		SetTimer(hWnd, 222, 1, nullptr);
+		currentX = 10;
+		currentY = 400;
+		}
+		break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+	case WM_TIMER:
+	{
+		if (isMoving)
+		{
+			bool xStop = false, yStop = false;
+			if (currentX < destinationX)
+			{
+				currentX += 1;
+			}
+			else if (currentX > destinationX)
+			{
+				currentX -= 1;
+			}
+			else {
+				xStop = true;
+			}
+			
+			if (currentY < destinationY)
+			{
+				currentY += 1;
+			}
+			else if (currentY > destinationY)
+			{
+				currentY -= 1;
+			}
+			else {
+				yStop = true;
+			}
+				
+			if (xStop && yStop)
+			{
+				isMoving = false;
+			}
+			
+			if (localAnimation == 10)
+			{
+				animation++;
+				localAnimation %= 10;
+			}
+			
+			localAnimation++;
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+	}
+	break;
+	case WM_LBUTTONDOWN:
+		{
+		isMoving = true;
+		destinationX = LOWORD(lParam);
+		destinationY = HIWORD(lParam);
+		}
+		break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+
+            switch (animation % 4)
+            {
+			case 0:
+				LoadAndBlitBitmap(__T("D:/Win API/WindowsProject4/WindowsProject4/Image1.bmp"), hdc, currentX, currentY);
+				break;
+
+			case 1:
+				LoadAndBlitBitmap(__T("D:/Win API/WindowsProject4/WindowsProject4/Image2.bmp"), hdc, currentX, currentY);
+				break;
+
+			case 2:
+				LoadAndBlitBitmap(__T("D:/Win API/WindowsProject4/WindowsProject4/Image3.bmp"), hdc, currentX, currentY);
+				break;
+
+			case 3:
+				LoadAndBlitBitmap(__T("D:/Win API/WindowsProject4/WindowsProject4/Image4.bmp"), hdc, currentX, currentY);
+				break;
+            }
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+		KillTimer(hWnd, 222);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+void LoadAndBlitBitmap(LPCWSTR fileName, HDC hdc, int x, int y)
+{
+	HBITMAP hBitmap;
+	hBitmap = (HBITMAP)LoadImage(NULL, fileName, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	HDC hLocalDC;
+	hLocalDC = CreateCompatibleDC(hdc);
+	BITMAP qBitmap;
+	GetObject(reinterpret_cast<HGDIOBJ>(hBitmap), sizeof(BITMAP), reinterpret_cast<LPVOID>(&qBitmap));
+
+	HBITMAP hOldBmp = (HBITMAP)SelectObject(hLocalDC, hBitmap);
+
+	BOOL qRetBlit = BitBlt(hdc, x, y, qBitmap.bmWidth, qBitmap.bmHeight, hLocalDC, 0, 0, SRCCOPY);
+	SelectObject(hLocalDC, hOldBmp);
+	DeleteDC(hLocalDC);
+	DeleteObject(hBitmap);
+}
